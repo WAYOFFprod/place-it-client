d8645545353ae33f2da6d1414ff639af63ee18b7
WAYOFFprod <raphael@wayoff.ch>
2024-07-09 20:03:52 +0200

    some basic responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -153,7 +153,7 @@
 		</div>
 	</Header>
 	<div class="overflow-y-scroll absolute bottom-0 top-[166px] w-full">
-		<div class="container mx-auto flex gap-8 py-8">
+		<div class="container mx-auto flex flex-col md:flex-row flex-wrap gap-8 py-8 px-8">
 			<!-- search -->
 			<TextInput
 				on:onChange={searchUpdated}
@@ -168,40 +168,42 @@
 					<img src="/svg/search.svg" alt="Search icon" />
 				</div>
 			</TextInput>
-
-			<ToggleButton
-				id="recent"
-				label="Récents"
-				class="hover:bg-fluorescent-cyan"
-				classInactive="bg-white"
-				classActive="!bg-fluorescent-cyan-focus"
-				on:change={toggleRecent}><img src="/svg/alarm.svg" alt="recent icon" /></ToggleButton
-			>
-			<ToggleButton
-				id="favorit"
-				label="Favoris"
-				class="hover:bg-tea-rose"
-				classInactive="bg-white"
-				classActive="!bg-tea-rose-focus"
-				on:change={toggleFavorit}><img src="/svg/heart.svg" alt="favorit icon" /></ToggleButton
-			>
+			<div class="flex gap-8 justify-center">
+				<ToggleButton
+					id="recent"
+					label="Récents"
+					class="hover:bg-fluorescent-cyan"
+					classInactive="bg-white"
+					classActive="!bg-fluorescent-cyan-focus"
+					on:change={toggleRecent}><img src="/svg/alarm.svg" alt="recent icon" /></ToggleButton
+				>
+				<ToggleButton
+					id="favorit"
+					label="Favoris"
+					class="hover:bg-tea-rose"
+					classInactive="bg-white"
+					classActive="!bg-tea-rose-focus"
+					disabled={!isConnected}
+					on:change={toggleFavorit}><img src="/svg/heart.svg" alt="favorit icon" /></ToggleButton
+				>
+			</div>
 			<Select class="min-w-52" id="canvaType" placeholder="Tous" options={canvaTypeOptions}
 			></Select>
-			<div class="grow flex flex-row-reverse">
+			<div class="grow flex justify-center md:justify-end">
 				<Button stretch={false} on:click={onCreateCanva}>
 					<img src="/svg/plus.svg" alt="plus icon" />
-					<span>Créer un nouveau canva</span>
+					<span class="inline lg:hidden xl:inline">Créer un nouveau canva</span>
 				</Button>
 			</div>
 		</div>
 		<!-- content -->
-		<div class="h-full items-center justify-center {canvas.length == 0 ? 'flex' : 'hidden'}">
+		<div class="h-full px-8 items-center justify-center {canvas.length == 0 ? 'flex' : 'hidden'}">
 			<span class="uppercase">Vous n'avez pas encore de canva</span>
 		</div>
 		<div
-			class="container mx-auto {canvas.length != 0
+			class="container mx-auto p-8 {canvas.length != 0
 				? 'flex'
-				: 'hidden'} flex-wrap gap-5 justify-between h-fit"
+				: 'hidden'} flex-wrap gap-5 justify-center h-fit"
 		>
 			{#each canvas as canva}
 				<CanvaPreview {canva}></CanvaPreview>
