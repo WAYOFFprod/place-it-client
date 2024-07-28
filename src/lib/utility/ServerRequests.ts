a948cbab65bb57324d9a5ac411471a171aaab4d3
WAYOFFprod <raphael@wayoff.ch>
2024-07-28 20:06:17 +0200

    back to include

--- src/lib/utility/ServerRequests.ts
+++ src/lib/utility/ServerRequests.ts
@@ -15,7 +15,7 @@ export default class ServerRequests {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
+        credentials: "include", // include, *same-origin, omit
         headers: this.headers,
         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(payload), // body data type must match "Content-Type" header
@@ -44,7 +44,7 @@ export default class ServerRequests {
           method: "GET", // *GET, POST, PUT, DELETE, etc.
           mode: "cors", // no-cors, *cors, same-origin
           cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, *same-origin, omit
+          credentials: "include", // include, *same-origin, omit
           headers: this.headers,
           referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         }
