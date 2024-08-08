2081d722bd6428fb0ca6e97b15b3d748118a37f4
WAYOFFprod <raphael@wayoff.ch>
2024-08-08 18:55:35 +0200

    updated toggle

--- src/lib/components/svg/toggle/background.svelte
+++ src/lib/components/svg/toggle/background.svelte
@@ -1,31 +1,25 @@
-<svg class={$$props.class} width="24" height="13" viewBox="0 0 24 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <rect x="4" y="0.5" width="16" height="1.33333" fill="#141414"/>
-  <rect x="4" y="11.1667" width="16" height="1.33333" fill="#141414"/>
-  <rect x="2.66699" y="1.83325" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect x="21.333" y="11.1667" width="1.33333" height="1.33333" transform="rotate(180 21.333 11.1667)" fill="#141414"/>
-  <rect x="1.33301" y="3.1665" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect x="22.667" y="9.8335" width="1.33333" height="1.33333" transform="rotate(180 22.667 9.8335)" fill="#141414"/>
-  <rect x="1.33301" y="8.50024" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect x="22.667" y="4.49976" width="1.33333" height="1.33333" transform="rotate(180 22.667 4.49976)" fill="#141414"/>
-  <rect y="4.49976" width="1.33333" height="4" fill="#141414"/>
-  <rect x="24" y="8.50024" width="1.33333" height="4" transform="rotate(180 24 8.50024)" fill="#141414"/>
-  <rect x="1.33301" y="4.49976" width="1.33333" height="4" fill="currentColor"/>
-  <rect x="2.66602" y="3.1665" width="1.33333" height="6.66667" fill="currentColor"/>
-  <rect x="4" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="5.33301" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="6.66602" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="8" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="9.33301" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="10.666" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="12" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="13.333" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="14.666" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="16" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="17.333" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="18.666" y="1.83325" width="1.33333" height="9.33333" fill="currentColor"/>
-  <rect x="20" y="3.1665" width="1.33333" height="6.66667" fill="currentColor"/>
-  <rect x="21.333" y="4.49976" width="1.33333" height="4" fill="currentColor"/>
-  <rect x="2.66699" y="9.8335" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect x="21.333" y="3.1665" width="1.33333" height="1.33333" transform="rotate(180 21.333 3.1665)" fill="#141414"/>
+<svg
+	class={$$props.class}
+	width="24"
+	height="12"
+	viewBox="0 0 24 12"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M24 4H22.67V2.67H21.33V1.33H20V0H4V1.33H2.67V2.67H1.33V4H0V8H1.33V9.33H2.67V10.67H4V12H20V10.67H21.33V9.33H22.67V8H24V4Z"
+		fill="currentColor"
+	/>
+	<path d="M20 0H4V1.33H20V0Z" fill="#141414" />
+	<path d="M20 10.67H4V12H20V10.67Z" fill="#141414" />
+	<path d="M3.99992 1.33H2.66992V2.66H3.99992V1.33Z" fill="#141414" />
+	<path d="M21.33 9.33H20V10.66H21.33V9.33Z" fill="#141414" />
+	<path d="M2.66008 2.67H1.33008V4H2.66008V2.67Z" fill="#141414" />
+	<path d="M22.6601 8H21.3301V9.33H22.6601V8Z" fill="#141414" />
+	<path d="M2.66008 8H1.33008V9.33H2.66008V8Z" fill="#141414" />
+	<path d="M22.6601 2.67H21.3301V4H22.6601V2.67Z" fill="#141414" />
+	<path d="M1.33 4H0V8H1.33V4Z" fill="#141414" />
+	<path d="M23.9999 4H22.6699V8H23.9999V4Z" fill="#141414" />
+	<path d="M3.99992 9.33H2.66992V10.66H3.99992V9.33Z" fill="#141414" />
+	<path d="M21.33 1.33H20V2.66H21.33V1.33Z" fill="#141414" />
 </svg>
-  
--- src/lib/components/svg/toggle/knob.svelte
+++ src/lib/components/svg/toggle/knob.svelte
@@ -1,22 +1,25 @@
-<svg class={$$props.class} width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <rect x="9.33301" y="11.1667" width="1.33333" height="1.33333" transform="rotate(180 9.33301 11.1667)" fill="#141414"/>
-  <rect x="8" y="12.5" width="4" height="1.33333" transform="rotate(180 8 12.5)" fill="#141414"/>
-  <rect x="2.66699" y="1.83325" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect x="2.66699" y="9.8335" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect x="1.33301" y="3.1665" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect x="1.33301" y="8.50024" width="1.33333" height="1.33333" fill="#141414"/>
-  <rect y="4.5" width="1.33333" height="4" fill="#141414"/>
-  <rect x="1.33301" y="4.5" width="1.33333" height="4" fill="#F6F4F4"/>
-  <rect x="9.33301" y="4.5" width="1.33333" height="4" fill="#F6F4F4"/>
-  <rect x="2.66699" y="3.1665" width="1.33333" height="6.66667" fill="#F6F4F4"/>
-  <rect x="8" y="3.1665" width="1.33333" height="6.66667" fill="#F6F4F4"/>
-  <rect x="4" y="1.83325" width="1.33333" height="9.33333" fill="#F6F4F4"/>
-  <rect x="5.33301" y="1.83325" width="1.33333" height="9.33333" fill="#F6F4F4"/>
-  <rect x="6.66699" y="1.83325" width="1.33333" height="9.33333" fill="#F6F4F4"/>
-  <rect x="8" y="1.83325" width="4" height="1.33333" transform="rotate(180 8 1.83325)" fill="#141414"/>
-  <rect x="9.33301" y="3.1665" width="1.33333" height="1.33333" transform="rotate(180 9.33301 3.1665)" fill="#141414"/>
-  <rect x="10.667" y="4.5" width="1.33333" height="1.33333" transform="rotate(180 10.667 4.5)" fill="#141414"/>
-  <rect x="12" y="8.50024" width="1.33333" height="4" transform="rotate(180 12 8.50024)" fill="#141414"/>
-  <rect x="10.667" y="9.8335" width="1.33333" height="1.33333" transform="rotate(180 10.667 9.8335)" fill="#141414"/>
+<svg
+	class={$$props.class}
+	width="12"
+	height="12"
+	viewBox="0 0 12 12"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M10.6701 4H9.33008V2.67H8.00008V1.33H6.67008H6.51008H5.33008H4.00008V2.67H2.67008V4H1.33008V8H2.67008V9.33H4.00008V10.67H5.33008H6.51008H6.67008H8.00008V9.33H9.33008V8H10.6701V4Z"
+		fill="#F6F4F4"
+	/>
+	<path d="M8 10.67H4V12H8V10.67Z" fill="#141414" />
+	<path d="M3.99992 1.33H2.66992V2.66H3.99992V1.33Z" fill="#141414" />
+	<path d="M3.99992 9.33H2.66992V10.66H3.99992V9.33Z" fill="#141414" />
+	<path d="M2.66008 2.67H1.33008V4H2.66008V2.67Z" fill="#141414" />
+	<path d="M2.66008 8H1.33008V9.33H2.66008V8Z" fill="#141414" />
+	<path d="M1.33 4H0V8H1.33V4Z" fill="#141414" />
+	<path d="M11.9999 4H10.6699V8H11.9999V4Z" fill="#141414" />
+	<path d="M8 0H4V1.33H8V0Z" fill="#141414" />
+	<path d="M9.33 1.33H8V2.66H9.33V1.33Z" fill="#141414" />
+	<path d="M10.6601 2.67H9.33008V4H10.6601V2.67Z" fill="#141414" />
+	<path d="M10.6601 8H9.33008V9.33H10.6601V8Z" fill="#141414" />
+	<path d="M9.33 9.33H8V10.66H9.33V9.33Z" fill="#141414" />
 </svg>
-  
