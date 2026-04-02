028c262f2db86affdcf0d4b645b9009f8c6e6230
Raphael Baumann <raphael@wayoff.ch>
2026-04-02 21:26:02 +0200

    refactor: layout only syncs canvas creations on reconnect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,15 @@
 	import '../app.css';
 	import { onMount } from 'svelte';
 	import { isOnline } from '$lib/stores/onlineStore';
-	import { syncPending } from '$lib/utility/OfflineSync';
+	import { syncPendingCreations } from '$lib/utility/OfflineSync';
 	import PwaInstall from '$lib/components/pwaInstall.svelte';
 
 	let previouslyOnline = true;
 
 	onMount(() => {
-		// Trigger a sync every time we come back online
 		const unsub = isOnline.subscribe((online) => {
 			if (online && !previouslyOnline) {
-				syncPending();
+				syncPendingCreations();
 			}
 			previouslyOnline = online;
 		});
